b61d8c69e9c71c25d735b31f31f8ad0933c92deb
MD. Sulaiman Hosain <solaiman.sh6@gmail.com>
2021-12-14 13:25:12 +0600

    Navigation Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from './Components/Navigation';
 
 function App() {
-	return <></>;
+	return (
+		<>
+			<Navigation logo={logo} />
+		</>
+	);
 }
 
 export default App;
