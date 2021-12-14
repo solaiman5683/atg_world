0e993d5754354872dcf3df25708c2a432568d1de
MD. Sulaiman Hosain <solaiman.sh6@gmail.com>
2021-12-14 14:09:41 +0600

    Header Component Created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navigation from './Components/Navigation';
+import Navigation from './Components/Navigation/Navigation';
+import Header from './Components/Header/Header';
 
 function App() {
 	return (
 		<>
 			<Navigation logo={logo} />
+			<Header />
 		</>
 	);
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './header.css';
+const Header = () => {
+	return (
+		<div className='header-area'>
+			<div className='wrapper'>
+				<div className='container'>
+					<div className='content'>
+						<h1>Computer Engineering</h1>
+						<p>142,765 Computer Engineers follow this</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
